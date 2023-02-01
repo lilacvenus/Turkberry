e2703b22c7a65253fd82a2843a41f40fa6401166
Ryan Isnor <w0457048@campus.nscc.ca>
2023-02-01 10:05:25 -0400

    Seeders complete... for now

---
+++ database/seeders/VarietySeeder.php
@@ -0,0 +1,414 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VarietySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('varieties')->insert([
+            'name' => 'Almond',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Amaretto',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Banana Cream',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Birthday Cake',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Bubble Gum',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Cappuccino',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Cheesecake',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Chocolate',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Chocoloco',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Creamsicle',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Forest Berries',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Green Apple',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Green Tea',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Lemon',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Lime',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Mango Tango',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Mint Chocolate',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Nutella',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Original Bight',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Peach Mango',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Peppermint Pattie',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Pineapple',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Pumpkin Spice',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Raspberry',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Red Velvet',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Salted Caramel',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Strawberry',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Sweet Watermelon',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Venetian Cream',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Frozen Yogurt',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Vanilla',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Milkshake',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Milk Chocolate',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Milkshake',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Dark Chocolate',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Milkshake',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Strawberry',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Milkshake',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Vanilla',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Cookies and Cream',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Cookie Craze',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Red Berry Cream',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'S\'mores',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Cookies and Cream',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Kit Kat Attack',
+            'description' => '',
+            'image' => '',
+            'availability' => true,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Mini Egg',
+            'description' => '',
+            'image' => '',
+            'availability' => false,
+            'type' => 'Hurricane',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Vanilla',
+            'description' => '',
+            'image' => '',
+            'availability' => false,
+            'type' => 'Donut',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Chocolate',
+            'description' => '',
+            'image' => '',
+            'availability' => false,
+            'type' => 'Donut',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Maple',
+            'description' => '',
+            'image' => '',
+            'availability' => false,
+            'type' => 'Donut',
+            'stock' => true
+        ]);
+
+        DB::table('varieties')->insert([
+            'name' => 'Caramel',
+            'description' => '',
+            'image' => '',
+            'availability' => false,
+            'type' => 'Donut',
+            'stock' => true
+        ]);
+    }
+}
