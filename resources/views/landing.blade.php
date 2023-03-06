989d79a074bf97d80b037c58904f7d932ab47e3e
AshtonBurgess <w0465511@nscc.ca>
2023-03-06 11:12:45 -0400

    changes being made to product, landing changes to carousel

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -19,38 +19,87 @@
 
 
 
-    <img src="{{ URL::asset('turkberry-assets/POS Screen/welcome.png') }}" >
-    <img src="{{ URL::asset('turkberry-assets/Milkshakes/H2.jpg') }}" >
-{{--                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">--}}
-{{--                                <div class="carousel-inner">--}}
-{{--                                    <div class="carousel-item active">--}}
-{{--                                        <img src="{{ URL::asset('turkberry-assets/POS Screen/welcome.png') }}" alt="First slide">--}}
-{{--                                    </div>--}}
-{{--                                    <div class="carousel-item">--}}
-{{--                                        <img src="{{ URL::asset('turkberry-assets/POS Screen/28.jpg') }}" alt="Second slide">--}}
-{{--                                    </div>--}}
-{{--                                    <div class="carousel-item">--}}
-{{--                                        <img src="{{ URL::asset('turkberry-assets/POS Screen/2.png') }}" alt="Third slide">--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">--}}
-{{--                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-{{--                                    <span class="sr-only">Previous</span>--}}
-{{--                                </a>--}}
-{{--                                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">--}}
-{{--                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-{{--                                    <span class="sr-only">Next</span>--}}
-{{--                                </a>--}}
-{{--                            </div>--}}
 
+{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">--}}
 
-{{--                        </div>--}}
+{{--    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
 
-
-
+{{--    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">--}}
+{{--        <ol class="carousel-indicators">--}}
+{{--            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>--}}
+{{--            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>--}}
+{{--            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>--}}
+{{--        </ol>--}}
+{{--        <div class="carousel-inner">--}}
+{{--            <div class="carousel-item active">--}}
+{{--                <img class="d-block w-100" src="{{ URL::asset('turkberry-assets/POS Screen/welcome.png') }}" alt="First slide">--}}
+{{--                <div class="carousel-caption d-none d-md-block">--}}
+{{--                    <h5>My Caption Title (1st Image)</h5>--}}
+{{--                    <p>The whole caption will only show up if the screen is at least medium size.</p>--}}
 {{--                </div>--}}
 {{--            </div>--}}
+
+{{--            <div class="carousel-item">--}}
+{{--                <img class="d-block w-100" src="{{ URL::asset('turkberry-assets/staff/2.jpg') }}" alt="Second slide">--}}
+{{--            </div>--}}
+{{--            <div class="carousel-item">--}}
+{{--                <img class="d-block w-100" src="{{ URL::asset('turkberry-assets/staff/2.jpg') }}" alt="Third slide">--}}
+{{--            </div>--}}
 {{--        </div>--}}
+{{--        <a class="carousel-control-prev fill=’%23c593d8" href="#carouselExampleIndicators" role="button" data-slide="prev">--}}
+{{--            <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
+{{--            <span class="sr-only">Previous</span>--}}
+{{--        </a>--}}
+{{--        <a class="carousel-control-next fill=’%23c593d8" href="#carouselExampleIndicators" role="button" data-slide="next">--}}
+{{--            <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
+{{--            <span class="sr-only">Next</span>--}}
+{{--        </a>--}}
 {{--    </div>--}}
+
+
+
+
+
+
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="{{ URL::asset('turkberry-assets/POS Screen/welcome.png') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="{{ URL::asset('turkberry-assets/SpecialPresentations/tazo.jpg') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{ URL::asset('turkberry-assets/SpecialPresentations/TurtleImage.jpg') }}" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </div>
 @endsection
