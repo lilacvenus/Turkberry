706e760cd8b11565c17e9264d9688798e9f49007
Samuel Cowie <w0456204@campus.nscc.ca>
2023-03-16 00:43:12 -0300

    Fresh Project -- First Commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,5 +19,20 @@
 
 
 
-ABOUT US!
+<p>ABOUT US!</p>
+    Turk Berry is a company that offers a wide range of delicious treats to satisfy your sweet tooth located in Canada.
+    From delectable donuts to creamy ice-cream, Turk Berry has something for everyone.
+    Our donuts are baked fresh every day, with a variety of flavors and toppings to choose from.
+    Whether you're in the mood for classic glazed donuts or something more unique like blueberry,
+    Turk Berry has got you covered.
+    Our donuts are always soft and fluffy, with the perfect balance of sweetness and flavor.
+    You can enjoy them on their own or pair them with a hot cup of coffee for a delicious breakfast or snack.
+    <br>
+    <br>
+    <p>In addition to our mouth-watering donuts, Turk Berry also offers a variety of creamy and refreshing ice-cream flavors.
+    Made with only the finest ingredients, our ice-cream is perfect for hot summer days or as a sweet treat after a long day.
+    With flavors unique Turk Berry's ice-cream is sure to satisfy any cravings. You can enjoy our ice-cream in a cone or cup,
+    or even take home a pint to enjoy at your leisure.
+    So if you're looking for delicious treats that will
+        make your taste buds dance with joy, be sure to check out Turk Berry today.</p>
 @endsection
