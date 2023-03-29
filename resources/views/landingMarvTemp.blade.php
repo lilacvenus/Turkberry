a1b32a0d5ac296b9051555c37ca16381dfe761a0
AshtonBurgess <w0465511@nscc.ca>
2023-03-29 08:53:59 -0300

    Landing Page

--- resources/views/landingMarvTemp.blade.php
+++ resources/views/landingMarvTemp.blade.php
@@ -1,9 +1,83 @@
 
 
-
 @extends('layouts.app')
 
 @section('content')
-fsdfgdfhgj
-@endsection
+    <main class="py-4"  style="background-color: rgb(CB1C8D)"
+{{--          style="background: url('turkberry-assets/pictures/27.jpg')"--}}
+    >
 
+
+    <div class="container"  >
+
+        {{--    <div class="row justify-content-center">--}}
+        {{--        <div class="col-md-8">--}}
+        {{--            <div class="card">--}}
+        {{--                <div class="card-header">{{ __('Admin') }}</div>--}}
+
+        {{--                <div class="card-body">--}}
+        {{--                    @if (session('status'))--}}
+        {{--                        <div class="alert alert-success" role="al url('turkberry-assets/pictures/27.jpg')ert">--}}
+        {{--                            {{ session('status') }}--}}
+        {{--                        </div>--}}
+        {{--                    @endif--}}
+        {{--                        <div class="table-responsive-sm">--}}
+
+
+
+
+
+        <div id="carouselExampleDark" class="carousel carousel-dark slide border border-dark" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" >
+                </button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label=""></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label=""></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="{{ URL::asset('turkberry-assets/POS Screen/welcomeScaled.png') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block"></div>
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="{{ URL::asset('turkberry-assets/SpecialPresentations/tazo.jpg') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block"></div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ URL::asset('turkberry-assets/SpecialPresentations/TurtleImage.jpg') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block"></div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+     
+        <div class="card-group">
+        <div class="card" style="width: 22rem;">
+            <img src="turkberry-assets/SpecialPresentations/easter.jpg" class="card-img-top" alt="easter">
+            <div class="card-body">
+                <h5 class="card-title">Easters Special</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+
+        </div>
+        <div class="card"  >
+            <img src="turkberry-assets/SpecialPresentations/promotion-milkshakes9.jpg" class="card-img-top" alt="promotion">
+            <div class="card-body">
+                <h5 class="card-title">Starting 2023 </h5>
+                <p class="card-text">We have the delicious vanilla , milk chocolate, dark chocolate, cookies and creams</p>
+            </div>
+
+        </div>
+        </div>
+    </div>
+    </main>
+@endsection
